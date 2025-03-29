9d548213ad17861d8af02a2e40fdcd91ead23552
maranix <ramanverma4183@gmail.com>
2025-03-30 01:44:36 +0530

    config(nvim/git): add more keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,7 +3,28 @@ return {
         'tpope/vim-fugitive',
         keys = function()
             return {
-                { '<leader>gs', vim.cmd.Git, desc = 'Open Git' },
+                { '<leader>gs', vim.cmd.Git, desc = 'open Git' },
+                {
+                    '<leader>p',
+                    function()
+                        vim.cmd.Git('push')
+                    end,
+                    desc = 'do git push',
+                },
+                {
+                    '<leader>P',
+                    function()
+                        vim.cmd.Git('pull')
+                    end,
+                    desc = 'do git pull',
+                },
+                {
+                    '<leader>PR',
+                    function()
+                        vim.cmd.Git({ 'pull', '--rebase' })
+                    end,
+                    desc = 'do git pull but rebase',
+                },
             }
         end,
     },
